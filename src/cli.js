1920cd67d00a1ef8e1ff6b6b70cb7e8c69bd6628
Travis CI User <travis@example.org>
2017-12-16 15:15:55 +0000

    publish: feat(deps): bump deps
    generated from commit adbe16e824c687b3fb9e8adacfbd07d33aac0987

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import program from 'caporal';
+import program, {FLOAT} from 'caporal';
 import updateNotifier from 'update-notifier';
 import pkg from '../package.json';
 import add from '.';
@@ -8,8 +8,8 @@ const notifier = updateNotifier({pkg});
 program.version(pkg.version)
   .description(pkg.description)
   .command('add', 'Add two numbers')
-  .argument('<a>', 'The first number', program.FLOAT)
-  .argument('<b>', 'The second number', program.FLOAT)
+  .argument('<a>', 'The first number', FLOAT)
+  .argument('<b>', 'The second number', FLOAT)
   .action(({a, b}, options, logger) => {
     logger.info(`${a} + ${b} = ${add(a, b)}`);
 
